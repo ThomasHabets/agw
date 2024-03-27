e48257f6b09cd86ad3ecfdd83a40851e2156def9
Thomas Habets <thomas@habets.se>
2024-03-27 21:36:47 +0000

    Revert "Fix last clippy"
    This reverts commit ce0682dc8546e5788739e7a1a9d185d8e6832060.

--- src/lib.rs
+++ src/lib.rs
@@ -115,6 +115,14 @@ impl Call {
         Ok(Call { bytes: arr })
     }
 
+    /// Create Call from string. Include SSID.
+    ///
+    /// Max length is 10, because that's the max length in the AGW
+    /// protocol.
+    pub fn from_str(s: &str) -> Result<Call> {
+        Self::from_bytes(s.as_bytes())
+    }
+
     /// Return true if the callsign is empty.
     ///
     /// Sometimes this is the correct thing, for incoming/outgoing AGW
@@ -127,13 +135,6 @@ impl Call {
             }
         }
         true
-    }
-}
-
-impl std::str::FromStr for Call {
-    type Err = anyhow::Error;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Self::from_bytes(s.as_bytes())
     }
 }
 
