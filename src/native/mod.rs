b991bdcb111757e68b2acd300d77ded26d2056b2
Thomas Habets <thomas@habets.se>
2024-07-28 22:32:52 +0100

    Remove dependency on libax25 just for ax25_aton_entry

--- src/native/mod.rs
+++ src/native/mod.rs
@@ -1,34 +1,40 @@
 use anyhow::Result;
 use libc::c_void;
-use std::ffi::CString;
 use std::io::{Error, ErrorKind, Read, Write};
-
-mod native {
-    // TODO: this can easily be rewritten in Rust.
-    #[link(name = "ax25", kind = "dylib")]
-    extern "C" {
-        pub fn ax25_aton_entry(cp: *const libc::c_char, axp: *mut libc::c_uchar) -> libc::c_int;
-    }
-}
 
 type BinaryCall = [u8; 7];
 fn empty_call() -> BinaryCall {
     [0u8; 7]
 }
 
+/// This is the same as ax25_aton_entry(), but spares us depending on libax25.
 pub fn parse_call(call: &str) -> Result<BinaryCall> {
-    let mut sax25_call = [0u8; 7];
-    let rc = unsafe {
-        native::ax25_aton_entry(
-            CString::new(call)?.as_ptr(),
-            &mut sax25_call as *mut libc::c_uchar,
-        )
+    let empty = b' ' << 1;
+    let mut bin = [empty; 7];
+    let split: Vec<_> = call.splitn(2, '-').collect();
+    let (call, ssid) = match split.len() {
+        1 => (split[0], 0),
+        2 => (split[0], split[1].parse::<u8>()?),
+        _ => panic!(),
     };
-    if rc == -1 {
-        Err(anyhow::Error::msg("failed to parse call {call}"))
-    } else {
-        Ok(sax25_call)
+    if ssid > 15 {
+        return Err(anyhow::Error::msg("SSID out of range in {call}"));
     }
+    if call.len() < 3 || call.len() > 6 {
+        return Err(anyhow::Error::msg("SSID out of range in {call}"));
+    }
+    for (i, ch) in call.chars().enumerate() {
+        if !ch.is_ascii_alphanumeric() {
+            return Err(anyhow::Error::msg("non-alphanum in {call}"));
+        }
+        let ch = ch
+            .to_uppercase()
+            .next()
+            .expect("internal error: character can't be uppercased");
+        bin[i] = (ch as u8) << 1;
+    }
+    bin[6] = ((ssid + b'0') << 1) & 0x1E;
+    Ok(bin)
 }
 
 struct FD {
@@ -202,3 +208,48 @@ impl Write for NativeStream {
 
 pub trait Stream: Read + Write {}
 impl Stream for NativeStream {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_good_calls() -> Result<()> {
+        for (call, want) in [
+            ("W2B", [174u8, 100, 132, 64, 64, 64, 0]),
+            ("M0THC2", [154u8, 96, 168, 144, 134, 100, 0]),
+            ("M0THC2-3", [154u8, 96, 168, 144, 134, 100, 6]),
+            ("M0THC2-15", [154u8, 96, 168, 144, 134, 100, 30]),
+            ("M0THC", [154u8, 96, 168, 144, 134, 64, 0]),
+            ("M0THC-0", [154u8, 96, 168, 144, 134, 64, 0]),
+            ("M0THC-1", [154u8, 96, 168, 144, 134, 64, 2]),
+            ("m0thc-1", [154u8, 96, 168, 144, 134, 64, 2]),
+            ("M0THC-2", [154u8, 96, 168, 144, 134, 64, 4]),
+            ("M0THC-15", [154u8, 96, 168, 144, 134, 64, 30]),
+        ] {
+            assert_eq!(want, parse_call(call)?, "failed for {call}");
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn parse_bad_calls() -> Result<()> {
+        for call in [
+            "",
+            "M",
+            "M0",
+            "-1",
+            "toolongcall",
+            "M0THC-16",
+            "M0THC-22",
+            "M0THC15",
+            "M0THC-",
+            "M0THC…",
+        ] {
+            if let Ok(v) = parse_call(call) {
+                panic!("succeeded for {call} into {v:?}, should fail");
+            }
+        }
+        Ok(())
+    }
+}
